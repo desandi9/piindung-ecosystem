e87768c57ac3ca4e6dc7c32f2d384320238f27e7
Desandi Herdiansyah <dsndihrdynsh@gmail.com>
2026-08-01 07:01:43 +0700

    feat: enhance login transition experience

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,27 @@
+"use client"
+
 import { LoginForm } from "@/components/piindung/login-form"
 import { PromoPanel } from "@/components/piindung/promo-panel"
+import { LoginTransitionScreen } from "@/components/piindung/login-transition-screen"
+import { useState } from "react"
 
 export default function LoginPage() {
-  return (
-    <main className="min-h-screen flex">
-      {/* Left Side - Login Form */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center p-6 lg:p-12 bg-white">
-        <LoginForm />
-      </div>
+  const [isTransitioning, setIsTransitioning] = useState(false)
 
-      {/* Right Side - Promotional Panel */}
-      <div className="hidden lg:block lg:w-1/2">
-        <PromoPanel />
-      </div>
-    </main>
+  return (
+    <>
+      {isTransitioning && <LoginTransitionScreen />}
+      <main className="min-h-screen flex">
+        {/* Left Side - Login Form */}
+        <div className="w-full lg:w-1/2 flex items-center justify-center p-6 lg:p-12 bg-white">
+          <LoginForm onTransitionStart={() => setIsTransitioning(true)} />
+        </div>
+
+        {/* Right Side - Promotional Panel */}
+        <div className="hidden lg:block lg:w-1/2">
+          <PromoPanel />
+        </div>
+      </main>
+    </>
   )
 }
